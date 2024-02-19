891293ed4f52e01fa3f4ed053944c57d622b0011
Pascalia Namwonja <pascaliamutua2@gmail.com>
2024-02-19 09:44:53 +0300

    python user_input

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+print("Hello {0}, you are {1} years old and live in {2}.".format(name,age,location))
